4c8e4057a2cdd6978d486c2cc533ced43a190eff
Jeremy Patrick <mrjepatrick@ymail.com>
2013-08-16 01:44:06 -0400

    Revert "Jeremy Patrick: Added search parameters, distance fields"
    This reverts commit bf2ba6dea58bcc087d4657ba027650af1a8bc7eb.

--- Tweet.php
+++ Tweet.php
@@ -24,7 +24,7 @@ class Tweet{
     var $iso_language_code;
     
     // Constructor
-    public function __construct($json, $near, $within){
+    public function __construct($json){
         $this->id = $json->id;
         $this->created_at = $json->created_at;
         $this->text = $json->text;
@@ -33,8 +33,12 @@ class Tweet{
         $this->geo = $json->geo;
         $this->coordinates = $json->coordinates;
         $this->iso_language_code = $json->metadata->iso_language_code;
-        $this->near = $near;
-        $this->within = $within;
+        
+//        $this->profile_image_url = mysql_real_escape_string($json->user->profile_image_url);
+//        $this->in_reply_to_user_id = $json->retweeted_status->in_reply_to_user_id;
+//        $this->user_id = $json->user->id;
+//        $this->user_name = $json->user->name;
+//        $this->user_created_at = $json->user->created_at;
     }
     
 }
