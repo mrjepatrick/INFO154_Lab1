ed0a6789e34b42c1a1ea56d1a83592c901861360
Edward Budihardjo <eyb23@drexel.edu>
2013-08-18 23:13:47 -0400

    Create Edward database.php

---
+++ Edward database.php
@@ -0,0 +1,14 @@
+ <?php
+ //connect to your database
+ mysql_connect("your.hostaddress.com", "within", "near") or die(mysql_error()); 
+ mysql_select_db("Database_Name") or die(mysql_error()); 
+ ?> 
+ 
+ CREATE TABLE within (name VARCHAR(6));
+ insert into insertTweets VALUES (5 mile, 15 mile, 25 mile)
+ 
+ Create TABLE near (name VARCHAR (30));
+ insert into insertTweets VALUES (Philadelphia, New Jersey, New York)
+ 
+ 
+ 
