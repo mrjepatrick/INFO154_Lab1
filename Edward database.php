dd6beabb0227b4c9dbfc30e9aaa3c54ab6e9c4cf
Edward Budihardjo <eyb23@drexel.edu>
2013-08-18 23:19:38 -0400

    Update Edward database.php

--- Edward database.php
+++ Edward database.php
@@ -4,11 +4,10 @@
  mysql_select_db("Database_Name") or die(mysql_error()); 
  ?> 
  
- CREATE TABLE within (name VARCHAR(6));
- insert into insertTweets VALUES (5 mile, 15 mile, 25 mile)
+ CREATE TABLE construct (within VARCHAR(6), near VARCHAR (30));
+ insert into construct VALUES ("5 mile", "15 mile", "25 mile"), ("Philadelphia", "New Jersey", "New York", "Delaware")
  
- Create TABLE near (name VARCHAR (30));
- insert into insertTweets VALUES (Philadelphia, New Jersey, New York)
+ 
  
  
  
